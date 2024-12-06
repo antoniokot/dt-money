7bb563140d797c6d1794680f920b9e39cfabd318
Antonio Kotsubo <antoniokotsubo@gmail.com>
2024-12-06 10:02:34 -0300

    final commit

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,5 +1,6 @@
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/TransactionsContext'
+import { useMemo } from 'react'
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
@@ -8,20 +9,22 @@ export function useSummary() {
 
   // percorre o array e reduze a uma outra estrutura de dados
   // { income: 0, outcome: 0, total: 0 }
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      acc.income += transaction.type === 'income' ? transaction.price : 0
-      acc.outcome += transaction.type === 'outcome' ? transaction.price : 0
-      acc.total += transaction.type === 'income' ? transaction.price : -transaction.price
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useMemo(() => {
+    transactions.reduce(
+      (acc, transaction) => {
+        acc.income += transaction.type === 'income' ? transaction.price : 0
+        acc.outcome += transaction.type === 'outcome' ? transaction.price : 0
+        acc.total += transaction.type === 'income' ? transaction.price : -transaction.price
+  
+        return acc
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
